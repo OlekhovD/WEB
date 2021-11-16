2545192bbbe0b7387aecbce63f5747d508db6ee6
OlekhovD <90377771+OlekhovD@users.noreply.github.com>
2021-11-16 11:48:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", function (event){});
+
+function calculate() {
+    let number = Number(document.getElementById("Clothes_Count").value);
+    let price = Number(document.getElementById("Clothes_Price").value);
+
+
+    let exp = new RegExp(/^[0-9]+$/i);
+    if (
+        !(
+            exp.test(number) && exp.test(price)
+        )
+    ) {
+        alert("Введите положительные числа");
+    } else if (number == "") {
+        alert("Введите количество одежды");
+    } else if (price == "") {
+        alert("Выберите цену одежды");
+    } else {
+        const cost = number * price;
+        document.getElementById("cost").innerHTML =
+            "Стоимость товаров составляет " + cost + "руб.";
+    }
+}
